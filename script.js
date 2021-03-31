7741aae3f3e72bc2958588deeba603d69304df30
ali-203 <62180010+ali-203@users.noreply.github.com>
2021-03-31 12:32:45 +0500

    updated java script

--- script.js
+++ script.js
@@ -55,8 +55,8 @@ const board_border = ' black';
      
       snakeboard_ctx.strokestyle = board_border;
       
-      snakeboard_ctx.fillRect(0, 0, snakeboard.width, snakeboard.height);
-      snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
+       snakeboard_ctx.fillRect(0, 0, snakeboard.width, snakeboard.height);
+       snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
     }
     
     
@@ -100,7 +100,7 @@ const board_border = ' black';
 
     function gen_food() {
       
-      food_x = random_food(0, snakeboard.width - 10);
+      food_x = random_food(0, snakeboard.width - 0);
       
       food_y = random_food(0, snakeboard.height - 10);
       
@@ -124,7 +124,7 @@ const board_border = ' black';
       const goingLeft = dx === -10;
       if (keyPressed === LEFT_KEY && !goingRight) {
         dx = -10;
-        dy = 0;
+         dy = 0;
       }
       if (keyPressed === UP_KEY && !goingDown) {
         dx = 0;
@@ -148,12 +148,13 @@ const board_border = ' black';
       const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;
       if (has_eaten_food) {
        
-        score += 5;
+         score += 5;
         
-        document.getElementById('score').innerHTML = score;
+         document.getElementById('score').innerHTML = score;
        
         gen_food();
       } else {
+       
        
         snake.pop();
       }
